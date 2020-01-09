fa12f9e9bca6d447d67cf1b1bd90ab37fedf27a3
shresthasamir4119 <shresthausui@icloud.com>
2020-01-09 12:47:07 +0545

    Updates

---
+++ precache-manifest.4b4310e2320fa264b8c6841fafb5f269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df05e3e46c02d5217ee8683aa5ccf8be",
+    "url": "/react-people-list/index.html"
+  },
+  {
+    "revision": "29fdd72f093af1f5c928",
+    "url": "/react-people-list/static/css/main.e82c372a.chunk.css"
+  },
+  {
+    "revision": "11fc9660b7a58b462351",
+    "url": "/react-people-list/static/js/2.7e330c6e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-people-list/static/js/2.7e330c6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29fdd72f093af1f5c928",
+    "url": "/react-people-list/static/js/main.04a39df3.chunk.js"
+  },
+  {
+    "revision": "1cb5dea5ceb9c69c079e",
+    "url": "/react-people-list/static/js/runtime-main.9c5ce451.js"
+  }
+]);
